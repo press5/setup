43fd73dc5c6d34c4dc68d10229e5d1648c0d4be8
Jessica K. Litwin <jessica@litw.in>
2020-05-28 20:44:23 -0400

    new bootstrap test

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,28 +4,37 @@
 # excuse my comments everywhere, this is a WIP and the comments will probably
 # evolve into something resembling actual documentation in addition to README.md
 
-# os detection of a sort
-if uname -s | grep -q Darwin; then
-  read -p "=== ensure you're logged in to the app store, then press enter ==="
-  /usr/bin/xcode-select --install   # install xcode 
-  read -p "=== press enter when your xcode install is done ==="
-  if ! [ -x "$(command -v brew)" ]; then
-    echo "\nbrew is not installed, let's install it"
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  fi
-  brew install ansible
-  # TODO: menu for tags, to run ansible-playbook right here? 
-fi 
+oschka=$(uname -s)
 
-
-
-# STILL TODO: 
-# four desktops in mission control
-# allow iTerm.app a11y permission
-# capslock -> ctrl (as it should be) 
-# Linux VM setup
-
-
-# TODO: linux 
-
-
+case $oschka in
+  Darwin)
+    read -p "=== checking for xcode: be logged into the app store, then press a key ==="
+    /usr/bin/xcode-select --install
+    read -p "when the installer is done, press a key"
+    if ! [ -x "$(command -v brew)" ]; then
+      echo "\nbrew isn't installed, let's fix that"
+      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+    brew install ansible
+    # end darwin
+    ;;
+  Linux)
+    #### FIXME   FIXME   FIXME   FIXME   WORK IN PROGRESS   FIXME   FIXME   FIXME   FIXME ####
+    # what breed of penguin are we
+    if ! [ -x "$(command -v lsb_release)" ]; then
+      penguin_type=$(lsb_release -is)
+      type_vers=$(lsb_release -cs)
+    fi
+    case $penguin_type in
+      Ubuntu)
+        apt update
+	apt install software-properties-common
+	apt-add-repository --yes --update ppa:ansible/ansible
+	apt install ansible
+        ;;
+      * )
+        echo "unsupported for now. :( install ansible."
+    esac
+    # end linux
+    ;;
+esac
